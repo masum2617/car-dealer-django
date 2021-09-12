9914971e8e9d06259c6d6a2bd6730af6f0e9cc2b
Masum Hasan <masum2617@gmail.com>
2021-09-13 00:32:25 +0600

    home page admin panel updated

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import Team
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    teams= Team.objects.all()
+    context = {
+        'teams':teams,
+    }
+    return render(request, 'pages/home.html', context)
+
+def about(request):
+
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
